88f3ea6849c0e92176bd4e8772778c0a8f993792
viacheslav <viacheslav.g@5ly.co>
2022-07-01 11:29:49 +0300

    first version

---
+++ main.py
@@ -0,0 +1,70 @@
+import math
+import sys  # sys нужен для передачи argv в QApplication
+from tcp import start_socket, stop_socket
+from socket import socket
+import threading
+
+class AdapterCore(): 
+    packet = b'd\x01\x00\x00\x00\x00'
+    sockets = []
+    serverSocket = 0
+    work = True
+
+    def __init__(self):
+        start_socket(self)
+        thread = threading.Thread(target=self.start_accepting)
+        thread.start()
+
+    def setup_text(self, text, title):
+        print(text, title)
+
+    def setup_packet(self, packet):
+        self.packet = packet
+        invalidSockects = []
+        for sock in self.sockets:
+            try:
+                sock.send(packet)
+            except:
+                invalidSockects.append(sock)   
+        for invSock in invalidSockects:
+            try:
+                self.sockets.remove(invSock)
+            except:
+                pass
+        print(len(self.sockets))
+            
+        # print(packet)
+
+    def start_accepting(self):
+        self.serverSocket = socket()
+        self.serverSocket.bind(('', 8536))
+        self.serverSocket.listen(10)
+        while self.work:
+            try:
+                s, addr = self.serverSocket.accept()
+                print('Connect: ',addr)
+                firstPacket = s.recv(2)
+                if firstPacket == b'd\x01':
+                    if (len(self.packet)):
+                        try:
+                            s.send(self.packet)
+                            self.sockets.append(s)
+                        except:
+                            pass
+            except:
+                pass
+
+    def end(self):
+        self.work = False
+        if (self.serverSocket):
+            self.serverSocket.close()
+
+
+def main():
+    exit = 'a'
+    adapter = AdapterCore()
+    while(exit != 'e'):
+        exit = input()
+    adapter.end()
+
+main()
---
+++ tcp.py
@@ -0,0 +1,126 @@
+import socket
+import sys
+import threading
+from time import sleep
+
+sock = 0
+work = True
+
+def get_int32_from_bytes(data):
+    res = int.from_bytes(data[0:4], 'big')
+    data = data[4:]
+    return data, res
+
+def get_string(data):
+    data, len = get_int32_from_bytes(data)
+    str = data[0:len].decode('utf-8')
+    data = data[len:]
+    return data, str
+
+def close_sock():
+    sock.close()
+
+def send_chords_frame():
+    global sock
+    frame = bytearray()
+    frame.append(100)
+    frame.append(1)
+    # print(frame)
+    sock.send(frame)
+    # print('Success')
+
+def init_socket():
+    global sock
+    address = sys.argv[-1]
+    print(address)
+    sock = socket.socket()
+    sock.connect((address, 8536))
+    # print(sock)
+    send_chords_frame()
+    
+    
+def get_text(set_text, set_packet):
+    if len(sys.argv) < 2:
+        print("Enter SOG-server ip-address as command line argument")
+        exit(1)
+
+    while work:
+        try:
+        # if True:
+            global sock
+            init_socket()
+            data = b''
+
+            while work:
+                packet = b''
+                packetPart = sock.recv(4)
+                data += packetPart
+                packet += packetPart
+                # print('\n\n\n', data)
+                data, flag = get_int32_from_bytes(data)
+                # print(flag)
+                if (flag == 1):
+                    packetPart = sock.recv(16 * 1024)
+                    data += packetPart
+                    packet += packetPart
+                    data, text = get_string(data)
+                    # print(text)
+                    data, title = get_string(data)
+                    # print(title)
+                    # print(set_text)
+                    set_text(text, title)
+                elif (flag == 0): 
+                    packetPart = sock.recv(8)
+                    data += packetPart
+                    packet += packetPart
+                    data, width = get_int32_from_bytes(data)
+                    # print(width)
+                    if (width > 0):
+                        data, height = get_int32_from_bytes(data)
+                        size = height * width
+                        inputSize = int((size + 3) / 4)
+                        while len(data) < inputSize:
+                            # print(len(data))
+                            packetPart = sock.recv(inputSize - len(data))
+                            data += packetPart
+                            packet += packetPart
+                            # print(len(data), inputSize)
+                        # print(data, width, height, inputSize)
+                        set_packet(packet)
+                        data = data[inputSize:]
+                    else:
+                        set_packet(packet)
+                else:
+                    print("Bad frame")
+                    break
+            close_sock()
+        except:        
+            print("connection error")
+            try:
+                close_sock()
+            except:
+                i = 0
+
+def ping_loop():
+    while work:
+        try:
+            send_chords_frame()
+            print('ping')
+        except:
+            pass
+        sleep(5)
+        
+
+def start_ping_thread():
+    thread = threading.Thread(target=ping_loop)
+    thread.start()
+
+def start_socket(par):
+    thread = threading.Thread(target=get_text, args=(par.setup_text, par.setup_packet))
+    thread.start()
+    start_ping_thread()
+    
+def stop_socket():
+    global work
+    work = False
+    close_sock()
